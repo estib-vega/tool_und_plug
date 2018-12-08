66a311efc904081578d85ee63af7d615a946a6cf
Jose Vega <jose.vega@here.com>
2018-12-08 15:38:02 +0100

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hi');
+const { read } = require('jimp')
+
+read('img/map.jpeg', (err, img) => {
+	if (err) console.log(err.message)
+
+	const width = img.bitmap.width
+	const height = img.bitmap.height
+
+	console.log(width, height)
+
+	img.pixelate(30).write('img/out.jpg')
+    
+})
