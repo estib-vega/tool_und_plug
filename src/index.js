c1da852e957cd940e8deabfc63da53b448981e9f
Jose Vega <jose.vega@here.com>
2018-12-09 17:47:45 +0100

    Refactored index.js, additions and changes to eslint.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
-const { read, RESIZE_BEZIER } = require('jimp')
+const { read, RESIZE_BEZIER, intToRGBA } = require('jimp')
 
-read('img/map.jpg', (err, img) => {
-	if (err) console.log(err.message)
+read('img/map.jpg')
+    .then(img => {
+        const width = img.bitmap.width
+        const height = img.bitmap.height
 
-	const width = img.bitmap.width
-	const height = img.bitmap.height
+        const minSize = width < height ? width : height
+        const pixelSize = Math.floor(minSize / 32)
+        const size = pixelSize * 32
 
-	const minSize = width < height ? width : height
-    const pixelSize = Math.floor(minSize / 32)
-    const size = pixelSize * 32
-    
-	img.resize(size, size)
-		.pixelate(pixelSize)
+        img.resize(size, size)
+            .pixelate(pixelSize)
         
-        
-    read('img/croc.jpg', (err, croc) => {
-        croc.resize(pixelSize, pixelSize, RESIZE_BEZIER)
-        for(let i = 0; i < 32; i++) {
-            for(let j = 0; j < 32; j++) {
-                img.composite(croc, i * pixelSize, j * pixelSize, {opacitySource: 0.65})
-            }
+        return {
+            img,
+            pixelSize,
         }
-        img.write('img/out.jpg')
-    })  
-})
+    })
+    .then(pixelImg => {
+        read('img/srcImg/croc.jpg')
+            .then(croc => {
+                croc.resize(pixelImg.pixelSize, pixelImg.pixelSize, RESIZE_BEZIER)
+
+                for(let i = 0; i < 32; i++) {
+                    for(let j = 0; j < 32; j++) {
+                        const copy = croc.clone()
+                        let x = i * pixelImg.pixelSize
+                        let y = j * pixelImg.pixelSize
+
+                        const color = pixelImg.img.getPixelColor(x, y)
+                        const { r, g, b } = intToRGBA(color)
+
+                        copy.color([{ apply: 'mix', params: [{ r, g, b }, 55] }])
+                        pixelImg.img.composite(copy, x, y)
+                    }
+                }
+                return pixelImg.img
+            })
+            .then(img => img.write('img/out.jpg'))
+    })
