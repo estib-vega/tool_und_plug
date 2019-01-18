f1372e78f74163f13dde14ce9eed908e417924df
Jose Vega <jose.vega@here.com>
2019-01-18 12:02:09 +0100

    Request module
    - Downloads 16 images for a given query
    - Cleaned up the code of the request
    
    TODO:
    	- Maybe allow user to set the number of downloaded images
    	- Check why some images won't get downloaded
    	- Maybe allow for multiple word queries

--- src/request.js
+++ src/request.js
@@ -6,27 +6,77 @@ const fs = require('fs-extra');
 
 const REQUEST_HEADER = { 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36' };
 const OUTPUT_DIR = 'downloaded_imgs';
+const IMAGE_DATA_CONTAINER_SELECTOR = 'div.rg_meta';
+const NUMBER_OF_DOWNLOADED_IMAGES = 16;
+const ALLOWED_IMAGE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const outputDirPath = path.join(process.cwd(), OUTPUT_DIR);
+
 const buildUrl = query => `https://www.google.co.in/search?q=${query}&source=lnms&tbm=isch`;
 
+const isAllowedImageType = type => ALLOWED_IMAGE_TYPES.includes(type.toLowerCase());
 
 const getGoogleImagesSiteFor = query => requestPromise({
     uri: buildUrl(query),
     headers: REQUEST_HEADER,
 });
 
-getGoogleImagesSiteFor('raccoon')
-    .then(response => {
-        const $ = cheerio.load(response);
-        const allDivs = $('div.rg_meta');
-        const imageDataString = allDivs['1'].children.find(child => child.type === 'text').data;
-        const imageData = JSON.parse(imageDataString);
+const getImageData = (allDivs, divId) => {
+    const imageDataString = allDivs[divId].children.find(child => child.type === 'text').data;
+    return JSON.parse(imageDataString);
+};
 
-        return { imageType: imageData.ity, imageUrl: imageData.ou };
-    }).then(({ imageType, imageUrl }) => {
-        const outputDirPath = path.join(process.cwd(), OUTPUT_DIR);
-        fs.mkdirpSync(outputDirPath);
-        const imageName = `outImage.${imageType}`;
-        const imagePath = path.join(outputDirPath, imageName);
+const setUpDownloadDirectory = () => {
+    fs.removeSync(outputDirPath);
+    fs.mkdirpSync(outputDirPath);
+};
 
-        request({ uri: imageUrl, headers: REQUEST_HEADER }).pipe(fs.createWriteStream(imagePath));
-    });
+const getAllDivs = response => {
+    const $ = cheerio.load(response);
+    return $(IMAGE_DATA_CONTAINER_SELECTOR);
+};
+
+const getImagesTypeAndUrlList = (response) => {
+    const allDivs = getAllDivs(response);
+    const imageDataList = [];
+    let indexShift = 1;
+
+    for(let i = 0; i < NUMBER_OF_DOWNLOADED_IMAGES; i++) {
+        const divId = String(i + indexShift);
+        const imageData = getImageData(allDivs, divId);
+        if(isAllowedImageType(imageData.ity))
+            imageDataList.push({ imageType: imageData.ity, imageUrl: imageData.ou });
+        else {
+            i--;
+            indexShift += 1;
+        }
+    }
+
+    return imageDataList;
+};
+
+const buildImagePath = (index, imageType) => {
+    const imageName = `${index + 1}.outImage.${imageType}`;
+    return path.join(outputDirPath, imageName);
+};
+
+const downloadSingleImage = ({ imageType, imageUrl }, index) =>
+    request({ uri: imageUrl, headers: REQUEST_HEADER })
+        .pipe(
+            fs.createWriteStream(
+                buildImagePath(index, imageType)
+            )
+        );
+
+const downloadImagesFromList = imageDataList =>
+    imageDataList.forEach(downloadSingleImage);
+
+const downloadImagesOf = searchQuery =>{
+    setUpDownloadDirectory();
+    getGoogleImagesSiteFor(searchQuery)
+        .then(getImagesTypeAndUrlList)
+        .then(downloadImagesFromList)
+        .catch(e => console.log(e.message));
+};
+
+module.exports = downloadImagesOf;
